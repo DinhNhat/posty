93a15d948ab2e66d86a866e09b045a591b197b87
Nhat Dinh <nguyendinhnhat206@gmail.com>
2021-08-06 17:54:53 -0400

    update form html

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,114 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
+
+<div class="flex items-center min-h-screen bg-gray-50">
+    <div class="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
+        <div class="flex flex-col md:flex-row">
+            <div class="h-32 md:h-auto md:w-1/2">
+                <img class="object-cover w-full h-full" src="https://source.unsplash.com/user/erondu/1600x900"
+                    alt="img" />
+            </div>
+            <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                <div class="w-full">
+                    <div class="flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-20 h-20 text-blue-600" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                        </svg>
+                    </div>
+                    <h1 class="mb-4 text-2xl font-bold text-center text-gray-700">
+                        Register
+                    </h1>
+
+                    <form action="{{ route('register') }}" method="post">
+                        @csrf
+
+                        <div class="mb-2">
+                            <label for="name" class="block text-sm sr-only">Name</label>
+                            <input type="text"
+                                name="name" id="name" placeholder="Your name"
+                                class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 @error('name') border-red-500 @enderror"/>
+                                
+                            @error('name')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        
+                        {{-- <div class="mb-4">
+                            <label for="name" class="sr-only">Name</label>
+                            <input type="text" name="name" id="name" placeholder="Your name"
+                                   class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ old('name') }}"
+                            />
+                            @error('name')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div> --}}
+                        <div class="mb-2">
+                            <label for="username" class="block text-sm sr-only">Username</label>
+                            <input type="text" name="username" id="username" placeholder="Username"
+                                   class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 @error('username') border-red-500 @enderror" value="{{ old('username') }}"
+                            />
+                            @error('username')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-2">
+                            <label for="email" class="block text-sm sr-only">Email</label>
+                            <input type="text" name="email" id="email" placeholder="Your email"
+                                   class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 @error('email') border-red-500 @enderror" value="{{ old('email') }}"
+                            />
+                            @error('email')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="password" class="block text-sm sr-only">Password</label>
+                            <input type="password" name="password" id="password" placeholder="Choose a password"
+                                   class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 @error('password') border-red-500 @enderror" value=""
+                            />
+                            @error('password')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-4">
+                            <label for="password_confirmation" class="block text-sm sr-only">Password again</label>
+                            <input type="password" name="password_confirmation" id="password_confirmation"
+                                   placeholder="Repeat your password"
+                                   class="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1" value=""
+                            />
+                        </div>
+                        <div>
+                            <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded
+                            font-medium w-full">Register</button>
+                        </div>
+                    </form>
+
+                    <div class="mt-4 text-center">
+                        <p class="text-sm">Don't have an account yet? <a href="#"
+                                class="text-blue-600 hover:underline"> Register.</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    {{-- <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded-lg">
             <form action="{{ route('register') }}" method="post">
                 @csrf
@@ -63,5 +170,5 @@
                 </div>
             </form>
         </div>
-    </div>
+    </div> --}}
 @endsection
